a25eb21a3fb112997d5822b90068ba653edb4cc0
Mark Tasaka <mark.tasaka@gmail.com>
2026-01-26 20:51:51 -0800

    KobBento.jsx layout updated to show side-by-side images

--- src/components/KoiBento.jsx
+++ src/components/KoiBento.jsx
@@ -4,49 +4,64 @@ import './css/Pages.css';
 
 const KoiBento = () => {
   const projectImages = [
+    // Paired images
     {
-      id: 1,
-      src: "/images/koi-bento/koi_bento_1.jpg",
-      alt: "Koi Bento 5 kg bucket mockup",
+      type: 'pair',
+      id: 'pair1',
+      images: [
+        {
+          src: "/images/koi-bento/koi_bento_1.jpg",
+          alt: "Koi Bento 5 kg bucket mockup"
+        },
+        {
+          src: "/images/koi-bento/koi_bento_2.jpg", 
+          alt: "Koi Bento 5 kg bucket label"
+        }
+      ],
       description: "Koi Bento 5 kg bucket: mock up (left) and label (right)"
     },
     {
-      id: 2,
-      src: "/images/koi-bento/koi_bento_2.jpg", 
-      alt: "Koi Bento 5 kg bucket label",
-      description: "" // No separate description for paired image
-    },
-    {
-      id: 3,
-      src: "/images/koi-bento/koi_bento_3.jpg",
-      alt: "Koi Bento 1 kg bottle mockup", 
+      type: 'pair',
+      id: 'pair2', 
+      images: [
+        {
+          src: "/images/koi-bento/koi_bento_3.jpg",
+          alt: "Koi Bento 1 kg bottle mockup"
+        },
+        {
+          src: "/images/koi-bento/koi_bento_4.jpg",
+          alt: "Koi Bento 1 kg bottle label"
+        }
+      ],
       description: "Koi Bento 1 kg bottle: mock up (left) and label (right)"
     },
+    // Single image
     {
-      id: 4,
-      src: "/images/koi-bento/koi_bento_4.jpg",
-      alt: "Koi Bento 1 kg bottle label",
-      description: "" // No separate description for paired image
-    },
-    {
+      type: 'single',
       id: 5,
       src: "/images/koi-bento/koi_bento_5.jpg",
       alt: "Koi Bento Instagram ads",
       description: "Koi Bento Instagram ads"
     },
+    // Paired images
     {
-      id: 6,
-      src: "/images/koi-bento/koi_bento_6.jpg",
-      alt: "Koi Bento t-shirt design",
+      type: 'pair',
+      id: 'pair3',
+      images: [
+        {
+          src: "/images/koi-bento/koi_bento_6.jpg",
+          alt: "Koi Bento t-shirt design"
+        },
+        {
+          src: "/images/koi-bento/koi_bento_7.jpg",
+          alt: "Koi Bento t-shirt mockup"
+        }
+      ],
       description: "Koi Bento t-shirts"
     },
+    // Single image
     {
-      id: 7,
-      src: "/images/koi-bento/koi_bento_7.jpg",
-      alt: "Koi Bento t-shirt mockup",
-      description: "" // No separate description for paired image
-    },
-    {
+      type: 'single',
       id: 8,
       src: "/images/koi-bento/koi_bento_8.jpg",
       alt: "Font used for Koi Bento project",
@@ -74,28 +89,66 @@ const KoiBento = () => {
 
             <div className="page-content">
               <div className="project-modules modules content">
-                {projectImages.map((image) => (
-                  <div key={image.id}>
-                    <div className="project-module module image">
-                      <div className="lightbox-link">
-                        <img 
-                          className="project-image"
-                          src={image.src}
-                          alt={image.alt}
-                        />
-                      </div>
-                    </div>
-                    
-                    {image.description && (
-                      <div className="project-module module text">
-                        <div className="rich-text module-text">
-                          <div>
-                            <span className="image-description">
-                              {image.description}
-                            </span>
+                {projectImages.map((item) => (
+                  <div key={item.id}>
+                    {item.type === 'pair' ? (
+                      // Paired images layout
+                      <div className="project-module module paired-images">
+                        <div className="image-pair-container">
+                          <div className="paired-image">
+                            <div className="lightbox-link">
+                              <img 
+                                className="project-image"
+                                src={item.images[0].src}
+                                alt={item.images[0].alt}
+                              />
+                            </div>
+                          </div>
+                          <div className="paired-image">
+                            <div className="lightbox-link">
+                              <img 
+                                className="project-image"
+                                src={item.images[1].src}
+                                alt={item.images[1].alt}
+                              />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="project-module module text">
+                          <div className="rich-text module-text">
+                            <div>
+                              <span className="image-description">
+                                {item.description}
+                              </span>
+                            </div>
                           </div>
                         </div>
                       </div>
+                    ) : (
+                      // Single image layout
+                      <>
+                        <div className="project-module module image">
+                          <div className="lightbox-link">
+                            <img 
+                              className="project-image"
+                              src={item.src}
+                              alt={item.alt}
+                            />
+                          </div>
+                        </div>
+                        
+                        {item.description && (
+                          <div className="project-module module text">
+                            <div className="rich-text module-text">
+                              <div>
+                                <span className="image-description">
+                                  {item.description}
+                                </span>
+                              </div>
+                            </div>
+                          </div>
+                        )}
+                      </>
                     )}
                   </div>
                 ))}

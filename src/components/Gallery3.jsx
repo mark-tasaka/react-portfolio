c816f764f57abdc0deb9f67744f49888fe4c5547
Mark Tasaka <mark.tasaka@gmail.com>
2026-02-02 21:50:57 -0800

    new images added; Gallery3.jsx updated

--- src/components/Gallery3.jsx
+++ src/components/Gallery3.jsx
@@ -9,85 +9,25 @@ const Gallery3 = () => {
     {
       type: 'single',
       id: 'single1',
-      src: "/images/art-gallery3/art1_1.jpg",
-      alt: "DCC Eastern Adventures Mock Up",
-      description: "A mock up of Dungeon Crawl Classics Eastern Adventures, a tabletop role-playing book I wrote and illustrated."
-    },
-    // Single image - art1_1.jpg
-    {
-      type: 'single',
-      id: 'single2',
-      src: "/images/art-gallery3/art1_2-4.jpg",
-      alt: "Eastern Adventures Cover Art",
-      description: "The cover art for Eastern themed role-playing books I designed.  I have published White Box Eastern Adventures (right) and am in the process of writing Red Box Eastern Adventures (left)."
+      src: "/images/art-gallery3/art3_1.jpg",
+      alt: "White Box Eastern Adventures Advanced Ed Mock Up",
+      description: "Photoshop mock up of the White Box Eastern Adventures Advanced Edition."
     },
     {
-      type: 'triple',
-      id: 'triple1',
+      type: 'pair',
+      id: 'pair1',
       images: [
         {
-          src: "/images/art-gallery3/art1_5.jpg",
-          alt: "DCC Eastern Adventures Character Sheet"
+          src: "/images/art-gallery3/art3_2.jpg",
+          alt: "White Box Eastern Adventures Advanced Ed Front Cover"
         },
         {
-          src: "/images/art-gallery3/art1_6.jpg",
-          alt: "Labrynth Lord Character Sheet"
-        },
-        {
-          src: "/images/art-gallery3/art1_7.jpg",
-          alt: "DCC Greenleaf Adventures Character Sheet"
+          src: "/images/art-gallery3/art3_3.jpg",
+          alt: "White Box Eastern Adventures Advanced Ed Back Cover"
         }
       ],
-      description: "Created with a digital pen in Photoshop, these Role-Playing Game character sheet have been design to capture the 'Old School' pencil-and-paper style of the early days of the RPG hobby."
+      description: "The cover illustration for White Box Eastern Adventures Advanced Edition.  Hand drawn with a digital pen in Photoshop."
     },
-    // Single image - art1_1.jpg
-    {
-      type: 'single',
-      id: 'single3',
-      src: "/images/art-gallery3/art1_8-10.jpg",
-      alt: "Pencil and Paper Sketches (Sohei Warrior Monks)",
-      description: "Digital art illustration created with Adobe Photoshop."
-    },
-    // Triple images - art1_11.jpg, art1_12.jpg, and art1_13.jpg
-    {
-      type: 'triple',
-      id: 'triple2',
-      images: [
-        {
-          src: "/images/art-gallery3/art1_11.jpg",
-          alt: "Beneath the Giant's Head Module Cover"
-        },
-        {
-          src: "/images/art-gallery3/art1_12.jpg",
-          alt: "The Cate and the Resurection Module Cover"
-        },
-        {
-          src: "/images/art-gallery3/art1_13.jpg",
-          alt: "Murt's Miscalculations Module Cover"
-        }
-      ],
-      description: "The cover art for three Dungeon Crawl Classics adventure books I wrote and illustrated.  Physical copies of these adventure modules are available to order on DriveThruRPG."
-    },
-    // Triple images - art1_14.jpg, art1_15.jpg, and art1_16.jpg
-    {
-      type: 'triple',
-      id: 'triple3',
-      images: [
-        {
-          src: "/images/art-gallery3/art1_14.jpg",
-          alt: "Mutant Crawl Classics Character Sheet"
-        },
-        {
-          src: "/images/art-gallery3/art1_15.jpg",
-          alt: "Basic Fantasy RPG Character Sheet"
-        },
-        {
-          src: "/images/art-gallery3/art1_16.jpg",
-          alt: "DCC Empire of the East Character Sheet"
-        }
-      ],
-      description: "Created with a digital pen in Photoshop, these Role-Playing Game character sheet have been design to capture the 'Old School' pencil-and-paper style of the early days of the RPG hobby."
-    }
   ];
 
   const projectTitle = "Art Gallery 3";

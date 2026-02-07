e950693bdcf2345e94f0c8b5a45588eff01ec8da
Mark Tasaka <mark.tasaka@gmail.com>
2026-02-06 21:13:46 -0800

    Gallery1 updated

--- src/components/Gallery1.jsx
+++ src/components/Gallery1.jsx
@@ -38,7 +38,7 @@ const Gallery1 = () => {
           alt: "DCC Greenleaf Adventures Character Sheet"
         }
       ],
-      description: "This section features hand-drawn illustrations I created either for my websites or for my published role-playing games.  The digital art was created with a digital pen and drawing tablet.  The pencil and paper illustrations are either conceptual sketches or interior illustrations for my game books."
+      description: "Created with a digital pen in Photoshop, these Role-Playing Game character sheet have been design to capture the 'Old School' pencil-and-paper style of the early days of the RPG hobby."
     },
     // Single image - art1_1.jpg
     {
@@ -91,7 +91,7 @@ const Gallery1 = () => {
   ];
 
   const projectTitle = "Art Gallery 1";
-  const projectDescription = `The digital art pieces in this gallery were hand-drawn with a digital pen tool in Adobe Photoshop. The illustrations are for my hobby role-playing game websites, and for a series of tabletop game books I published.`;
+  const projectDescription = `This section features hand-drawn illustrations I created either for my websites or for my published role-playing games.  The digital art was created with a digital pen and drawing tablet.  The pencil and paper illustrations are either conceptual sketches or interior illustrations for my game books.`;
 
   // Update document title when component mounts
   useEffect(() => {
